b828ee1a8999f03ac6feae092d19c88262f40cca
The Monad <153191651+themonadmemory@users.noreply.github.com>
2024-01-31 18:44:25 +0400

    Update script.js

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ var manualMemories = [
     { lat: 38.907, lng: -77.037, message: "Alpha test" },
     { lat: 34.0522, lng: -118.2437, message: "Beta test" },
     { lat: 40.3506, lng: -77.5634, message: "test" },
-    { Lat: -21.2426, Lng: 55.7064, message: "Welcome to MEMORIA, where the first memory is the beginning of a collective story. Monad" }
+    { lat: -21.2426, lng: 55.7064, message: "Welcome to MEMORIA, where the first memory is the beginning of a collective story. Monad" }
     // Ajoutez d'autres souvenirs manuels au besoin
 ];
 
