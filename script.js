cf5e19ce6ad6e5c27b48eab9660b51402a87a38e
The Monad <153191651+themonadmemory@users.noreply.github.com>
2024-01-31 17:54:01 +0400

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ var souvenirs = [
 var manualMemories = [
     { lat: 38.907, lng: -77.037, message: "Alpha test" },
     { lat: 34.0522, lng: -118.2437, message: "Beta test" }
-    { lat: 40.350606065486744, lng: -77.5634238370936, message: "test" }
+    { lat: 40.350606065486744, lng: -77.5634238370936, message: "test" },
     // Ajoutez d'autres souvenirs manuels au besoin
 ];
 
