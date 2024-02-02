2db7cc123f9a4cc6a5d46514f5a4bd86b913d3cf
The Monad <153191651+themonadmemory@users.noreply.github.com>
2024-02-02 14:33:04 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-// script.js
-
 const addMemoriesLink = '<p id="addMemoriesLink" class="popup-link">Add your memories here</p>';
 const addMemoriesLinkLarge = '<p id="addMemoriesLink" class="popup-link-large">Add your memories here</p>';
 
@@ -10,14 +8,14 @@ var souvenirs = [
 var manualMemories = [
     { lat: -20.3975, lng: 56.4365, message: "Ahoï !!! MEMORIA is an innovative platform designed to facilitate the exploration and sharing of memories worldwide. This interactive map-based application allows users to pinpoint and relive significant moments by adding their own memories to specific locations. On a desktop, right-click on the map to add a memory. On mobile, (landcape mode), tap and hold on the map to add a memory. A popup will appear, allowing users to click on ADD YOUR MEMORIES HERE." },
     { lat: 38.907, lng: -77.037, message: "On a desktop, right-click on the map to add a memory. On mobile, tap and hold on the map to add a memory. A popup will appear, allowing users to click on ADD YOUR MEMORIES HERE." },
-    { lat: 36.157, lng: -82.728, message: "Shia Laboeuf VS Anons. Anons win with frog sound. Capture the flag. For the glorious favor of kek." }
+    { lat: 36.157, lng: -82.728, message: "Shia Laboeuf VS Anons. Anons win with frog sound. Capture the flag. For the glorious favor of kek." },
     // Ajoutez d'autres souvenirs manuels au besoin
 ];
 
 var lastClickedCoords;
 var map = L.map('interactive-map');
-map.setView([manualMemories[0].lat, manualMemories[0].lng], 4); // Centrer sur le premier marqueur
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+map.setZoom(4); // Définir le niveau de zoom initial
 
 // Ajoutez les souvenirs existants
 souvenirs.forEach(function (souvenir) {
@@ -40,13 +38,8 @@ map.on('contextmenu', function (event) {
         .openOn(map);
 
     $('#addMemoriesLink').on('click', function () {
-        // Appeler une fonction pour pré-remplir le formulaire avec les coordonnées
         fillFormWithCoordinates(lastClickedCoords.lat, lastClickedCoords.lng);
-
-        // Rediriger vers le formulaire
         redirectToForm();
-
-        // Supprimer l'événement click pour éviter les appels multiples
         $('#addMemoriesLink').off('click');
     });
 });
@@ -62,37 +55,28 @@ map.on('taphold', function (event) {
         .openOn(map);
 
     $('#addMemoriesLink').on('click', function () {
-        // Appeler une fonction pour pré-remplir le formulaire avec les coordonnées
         fillFormWithCoordinates(lastClickedCoords.lat, lastClickedCoords.lng);
-
-        // Rediriger vers le formulaire
         redirectToForm();
-
-        // Supprimer l'événement click pour éviter les appels multiples
         $('#addMemoriesLink').off('click');
     });
 });
 
 function createMarker(souvenir, isFirstMarker) {
-    var markerColor = isFirstMarker ? 'black' : 'blue'; // Noir pour le premier, bleu pour les autres
+    var markerColor = isFirstMarker ? 'black' : 'blue';
     var marker = L.marker([souvenir.lat, souvenir.lng], { icon: L.divIcon({ className: 'custom-marker', iconSize: [20, 20], html: '<i style="color:' + markerColor + ';" class="fas fa-map-marker-alt"></i>' }) }).addTo(map);
 
     marker.bindPopup(souvenir.message);
     marker.on('click', function () {
-        // Afficher la bulle/fenêtre de lecture sans rediriger vers le formulaire
         marker.openPopup();
     });
 }
 
 function fillFormWithCoordinates(lat, lng) {
-    // Remplir le champ texte du formulaire avec les coordonnées
     var memoryText = 'Coordinates: Lat=' + lat + ', Lng=' + lng + ' (' + lat + ', ' + lng + ')';
-    // Vous pouvez ajouter d'autres informations ou instructions ici si nécessaire
     $('#memoryText').val(memoryText);
 }
 
 function redirectToForm() {
-    // Rediriger vers le formulaire seulement si la fenêtre "Add your memories here" est cliquée
     window.location.href = 'submit_memory.html?lat=' + lastClickedCoords.lat + '&lng=' + lastClickedCoords.lng;
 }
 
